fceb5a8e29956a86c324dbb7fac1ff2cdccc172d
Lucas Oliveira <lucas.pereira_oli@hotmail.com>
2022-01-05 22:08:21 -0300

    vendo published

---
+++ config/modular.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Configuration File
+|--------------------------------------------------------------------------
+|
+| You can overwrite default configuration here according to your app requirements.
+|
+*/
+return [
+    "i18n"     => false,
+    "https"    => false,
+    "modules"  => [
+
+    ]
+];
